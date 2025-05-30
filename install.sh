27626dfef8c658c5367657b2da22a77ea551ad61
grubbauer <raphael.grubbauer@hotmail.com>
2025-05-30 12:48:38 +0200

    Added install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p ~/.config/fastfetch
+# Cleans the directory
+rm -rf ~/.config/fastfetch/*
+
+# Now comes the installing part
+cp ./src/* ~/.config/fastfetch/
+cp ./LICENSE ~/.config/fastfetch/LICENSE
+
+echo "Succeded"
